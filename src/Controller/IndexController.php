51f665a3c637c48d62cb450c374c96306043e2c1
totor <toto@toot.fr>
2020-04-22 00:37:15 +0200

    Ajout de la pagination des commentaire et des post

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use App\Mercure\CookieGenerator;
+use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
 use App\Repository\MaillingListRepository;
 use Doctrine\ORM\EntityManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\CurlHttpClient;
@@ -29,13 +31,22 @@ class IndexController extends AbstractController
      * @Template("XxxYyyBundle:Front/post:index.html.twig")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(CookieGenerator $cookieGenerator, PostRepository $postrepo): Response
+    public function index(CookieGenerator $cookieGenerator, PostRepository $postrepo, Request $request, PaginatorInterface $paginator): Response
     {
 
         $posts = $postrepo->findAll(); //On récupère les posts
+
+        $posts = $paginator->paginate(
+            $posts, //Donnée a paginé
+            $request->query->getInt('page', 1), //Numéros de la page courante est 1 par default
+        3
+        );
+
+
         $response = $this->render('posts/index.html.twig', [
             'current_menu' => 'posts',
             'posts' => $posts,
+
         ]);
         if ($this->getUser()) {
             $response->headers->set('set-cookie', $cookieGenerator->generate($this->getUser()));
@@ -46,7 +57,6 @@ class IndexController extends AbstractController
 
 
     }
-
 
 
     # get success response from recaptcha and return it to controller
@@ -78,7 +88,6 @@ class IndexController extends AbstractController
     }
 
 
-
     /**
      * @Route("/sitemaps.xml", name="sitemap")
      */
@@ -97,8 +106,6 @@ class IndexController extends AbstractController
         foreach ($posts as $post) {
             $urls[] = ['loc' => $this->get('router')->generate('post.show', ['slug' => $post->getSlug()]), 'changefreq' => 'weekly', 'priority' => '1.0'];
         }
-
-
 
 
         // Once our array is filled, we define the controller response
