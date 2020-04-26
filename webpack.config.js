7cbba595a75952f24c4f0a66fa989a0b5328df8c
totor <toto@toot.fr>
2020-04-26 20:17:52 +0200

    ajout du cache offline correctif

--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,7 @@ Encore
             swDest: './../service-worker2.js',
             clientsClaim: true,
             skipWaiting: true,
-            exclude: ["https://s-website.ga/.well-known/mercure?topic=%2Fmessage'"],
+            exclude: ["https://s-website.ga/.well-known/.*"],
             runtimeCaching: [{
                 urlPattern: new RegExp(''),
                 handler: 'StaleWhileRevalidate'
