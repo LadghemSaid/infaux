c272ce8c67c54ce71739d36b708daa675e70fc56
totor <toto@toot.fr>
2020-05-09 01:51:10 +0200

    Ajout d'ajax sur le formulaire des post / correction notif

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -47,6 +47,9 @@ class CommentsController extends AbstractController
      */
     public function add(Request $req, $id, PostRepository $postRepo, Security $security)
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+
         $comment = new Comment();
         $user =$this->getUser();
 
@@ -75,7 +78,7 @@ class CommentsController extends AbstractController
 
 
         //$this->addFlash('success', "Commentaire ajouté avec succés :)");
-       return  $response = $this->render('comment/show.html.twig', [
+       return  $response = $this->render('comment/only-comment.html.twig', [
             'comment' => $comment,
 
         ]);
@@ -117,31 +120,6 @@ class CommentsController extends AbstractController
 
         }
         return new Response;
-    }
-
-    /**
-     * @Route("/post/{post}", name="paginate")
-     * @param Request $request
-     * @param PaginatorInterface $paginator
-     * @param $commentsrepo
-     * @return Response
-     */
-    public function getPaginateComment(Request $request, PaginatorInterface $paginator, CommentRepository $commentsrepo, $post)
-    {
-
-        $commentsToPaginate = $commentsrepo->findByPostField($post); //On récupère les commentaire du post
-        $comments = $paginator->paginate(
-            $commentsToPaginate, //Donnée a paginé
-            $request->query->getInt('page', 1), //Numéros de la page courante est 1 par default
-            5
-        );
-
-        $response = $this->render('comment/index.html.twig', [
-            'current_menu' => 'comments',
-            'comments' => $comments,
-        ]);
-        return $response;
-
     }
 
 }
