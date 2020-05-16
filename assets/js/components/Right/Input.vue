bc7c5e0bc16941fc37dccfe584bd9f9eefb33c43
Cloelh <cloelh@outlook.fr>
2020-05-16 21:40:57 +0200

    chat

--- assets/js/components/Right/Input.vue
+++ assets/js/components/Right/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Typing area -->
-        <div class="input-group">
+        <div class="input-group input-msg">
             <input type="text" placeholder="Votre message ..." v-model="content" @keyup.enter="sendMessage" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light">
             <div class="input-group-append">
                 <a id="button-addon2"  @click="sendMessage"  class="btn btn-link"> <i class="fa fa-paper-plane"></i></a>
